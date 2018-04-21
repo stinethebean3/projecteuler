d2993954852b9df384760551c4159854c10de004
Christine Vick <cdevlin23.cd@gmail.com>
2018-04-20 20:05:19 -0700

    second project euler problem

---
+++ euler2.py
@@ -0,0 +1,26 @@
+#!/bin/python
+
+out=0
+
+#fibonacci code here
+old1=1
+old2=2
+
+#def fib(num):
+current=3
+while current < 4000000:
+    print(current) 3
+    temp=current 3
+    current=old1+old2 5=3+2
+    old1=3
+    old2=2
+    
+
+
+# F(n) = F(n-1) + F(n-2)
+
+#for i in range (2, 4000000):
+ #   if i % 2 == 0:
+  #      out = out + i
+
+#print(out)
